412557017538826b55074be5c98f34dbb13de885
BlackWolfff <BlackBirdBrony@wp.pl>
2017-08-05 18:06:33 +0200

    you saw nothing

--- main.js
+++ main.js
@@ -2,11 +2,3 @@ const self = new Object({src: {}})
 const main = require("./main.json")
 for(const part in main.src) self.src[part] = require(`./${main.src[part]}`)
 eval(main.init)
-
-/*
-self.cmdHandler.isCommand = msg => {
-    if(!msg.content.startsWith(self.config.prefix)) return [false];
-    if(msg.author.bot) return [false, null];
-    let args = msg.content.slice(self.config.prefix.length).split(' ');
-    return [true, args[0], args.slice(1)];
-}*/
