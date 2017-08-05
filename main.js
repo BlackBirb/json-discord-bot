ea6a77f9ba6f289796375f67e46564754be96e75
BlackWolfff <BlackBirdBrony@wp.pl>
2017-08-05 18:05:19 +0200

    first command, fully working command handler, command registry and probably milion bugs. also ping command

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const main = require("./main.json")
 for(const part in main.src) self.src[part] = require(`./${main.src[part]}`)
 eval(main.init)
 
-
-
-
+/*
+self.cmdHandler.isCommand = msg => {
+    if(!msg.content.startsWith(self.config.prefix)) return [false];
+    if(msg.author.bot) return [false, null];
+    let args = msg.content.slice(self.config.prefix.length).split(' ');
+    return [true, args[0], args.slice(1)];
+}*/
