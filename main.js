4d0227829ef5acc2a13819102bb824c46cc43975
BlackWolfff <BlackBirdBrony@wp.pl>
2017-08-05 17:04:39 +0200

    adding simple command handler

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-const self = {};
+const self = new Object({src: {}})
+const main = require("./main.json")
+for(const part in main.src) self.src[part] = require(`./${main.src[part]}`)
+eval(main.init)
 
-eval(require("./main.json").init)
+
+
+
